e2993826837aec4db3e367ad1b513f124e128f3b
DJIRE ANGE FORTUNE <fortunedjire@gmail.com>
2026-09-08 14:17:55 +0000

    fix cache images ong

--- next.config.mjs
+++ next.config.mjs
@@ -22,6 +22,19 @@ const nextConfig = {
     compiler: "modern",
     silenceDeprecations: ["legacy-js-api"],
   },
+  async headers() {
+    return [
+      {
+        source: "/images/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=0, must-revalidate",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default withMDX(nextConfig);
